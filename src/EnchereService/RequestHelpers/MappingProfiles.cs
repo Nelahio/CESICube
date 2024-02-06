cd3b7fcd3d93832ade855e010f56d49fe9e5b970
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-02-06 10:33:45 +0100

    ajout mapping profiles automapper

---
+++ src/EnchereService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using EnchereService.DTOs;
+using EnchereService.Entities;
+
+namespace EnchereService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Enchere, EnchereDto>().IncludeMembers(x => x.Produit);
+        CreateMap<Produit, EnchereDto>();
+        CreateMap<CreateEnchereDto, Enchere>()
+        .ForMember(d => d.Produit, o => o.MapFrom(s => s));
+        CreateMap<CreateEnchereDto, Produit>();
+    }
+}
