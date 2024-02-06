ebc829da0d2ba4bf6fb9880cbecfb651134f4cf9
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-02-06 16:04:43 +0100

    ajout de la connexion a mongodb

---
+++ src/RechercheService/Models/Produit.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Entities;
+
+namespace RechercheService.Models;
+
+public class Produit : Entity
+{
+    public int ReservePrice { get; set; } = 0;
+    public string Seller { get; set; }
+    public string Winner { get; set; }
+    public int SoldAmount { get; set; }
+    public int CurrentHighBid { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime AuctionEnd { get; set; }
+    public string Statut { get; set; }
+    public string Make { get; set; }
+    public string ProductName { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public int? Size { get; set; }
+    public string Comments { get; set; }
+    public string ImageUrl { get; set; }
+}
--- src/RechercheService/Program.cs
+++ src/RechercheService/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Http.Features;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using RechercheService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,5 +15,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("RechercheDb", MongoClientSettings
+.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+await DB.Index<Produit>()
+.Key(x => x.Make, KeyType.Text)
+.Key(x => x.ProductName, KeyType.Text)
+.Key(x => x.Color, KeyType.Text)
+.CreateAsync();
 
 app.Run();
