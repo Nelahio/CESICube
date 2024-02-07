a182236e21844b7fe881d0888bfbd20f62ae90f1
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-02-07 09:53:51 +0100

    ajout du controller pour le ms recherche et requête get

---
+++ src/RechercheService/Controllers/RechercheController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using RechercheService.Models;
+
+namespace RechercheService.Controllers;
+
+[ApiController]
+[Route("api/recherche")]
+public class RechercheController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Produit>>> RechercheProduits(string searchTerm)
+    {
+        var query = DB.Find<Produit>();
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+
+        var result = await query.ExecuteAsync();
+
+        return result;
+    }
+}
