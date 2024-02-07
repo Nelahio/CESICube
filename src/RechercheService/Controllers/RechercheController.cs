197dc843ae7809a54a20270b104ea511e7cf1170
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-02-07 11:32:54 +0100

    ajout des filtres

--- src/RechercheService/Controllers/RechercheController.cs
+++ src/RechercheService/Controllers/RechercheController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using RechercheService.Models;
+using RechercheService.RequestHelpers;
 
 namespace RechercheService.Controllers;
 
@@ -9,18 +10,40 @@ namespace RechercheService.Controllers;
 public class RechercheController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Produit>>> RechercheProduits(string searchTerm, int pageNumber = 1, int pageSize = 4)
+    public async Task<ActionResult<List<Produit>>> RechercheProduits([FromQuery] RechercheParams rechercheParams)
     {
-        var query = DB.PagedSearch<Produit>();
-        query.Sort(x => x.Ascending(a => a.Make));
+        var query = DB.PagedSearch<Produit, Produit>();
 
-        if (!string.IsNullOrEmpty(searchTerm))
+        if (!string.IsNullOrEmpty(rechercheParams.SearchTerm))
         {
-            query.Match(Search.Full, searchTerm).SortByTextScore();
+            query.Match(Search.Full, rechercheParams.SearchTerm).SortByTextScore();
         }
 
-        query.PageNumber(pageNumber);
-        query.PageSize(pageSize);
+        query = rechercheParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+        };
+
+        query = rechercheParams.FilterBy switch
+        {
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+        };
+
+        if (!string.IsNullOrEmpty(rechercheParams.Seller))
+        {
+            query.Match(x => x.Seller == rechercheParams.Seller);
+        }
+        if (!string.IsNullOrEmpty(rechercheParams.Winner))
+        {
+            query.Match(x => x.Winner == rechercheParams.Winner);
+        }
+
+        query.PageNumber(rechercheParams.PageNumber);
+        query.PageSize(rechercheParams.PageSize);
 
         var result = await query.ExecuteAsync();
 
