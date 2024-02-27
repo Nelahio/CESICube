e006deeb7cc873ac188077745ce9c0c1467f7f94
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-02-27 13:34:04 +0100

    ajout des contrats

---
+++ src/Contracts/EnchereFinished.cs
@@ -0,0 +1,10 @@
+﻿namespace Contracts;
+
+public class EnchereFinished
+{
+    public bool ItemSold { get; set; }
+    public string EnchereId { get; set; }
+    public string Winner { get; set; }
+    public string Seller { get; set; }
+    public int? Amount { get; set; }
+}
