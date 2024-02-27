2cc78d84a1fc090bb312621a9e5e5aac89c000bb
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-02-27 14:57:51 +0100

    ajout des consumers

---
+++ src/EnchereService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using Contracts;
+using EnchereService.Data;
+using MassTransit;
+
+namespace EnchereService;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly EnchereDbContext _dbContext;
+
+    public BidPlacedConsumer(EnchereDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consume bid placed");
+
+        var enchere = await _dbContext.Encheres.FindAsync(context.Message.EnchereId);
+
+        if (enchere.CurrentHighBid == null
+        || context.Message.BidStatus.Contains("Accepted")
+        && context.Message.Amount > enchere.CurrentHighBid)
+        {
+            enchere.CurrentHighBid = context.Message.Amount;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/EnchereService/Consumers/EnchereFinishedConsumer.cs
@@ -0,0 +1,34 @@
+﻿using Contracts;
+using EnchereService.Data;
+using EnchereService.Entities;
+using MassTransit;
+using Microsoft.VisualBasic;
+
+namespace EnchereService;
+
+public class EnchereFinishedConsumer : IConsumer<EnchereFinished>
+{
+    private readonly EnchereDbContext _dbContext;
+
+    public EnchereFinishedConsumer(EnchereDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task Consume(ConsumeContext<EnchereFinished> context)
+    {
+        Console.WriteLine("--> Consuming enchere finished");
+
+        var enchere = await _dbContext.Encheres.FindAsync(context.Message.EnchereId);
+
+        if (context.Message.ItemSold)
+        {
+            enchere.Winner = context.Message.Winner;
+            enchere.SoldAmount = context.Message.Amount;
+        }
+
+        enchere.Statut = enchere.SoldAmount > enchere.ReservePrice
+        ? Statut.Finish : Statut.ReserveNotMet;
+
+        await _dbContext.SaveChangesAsync();
+    }
+}
