d4d15de69a3cc545c97409093ab58e1ffac54746
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-03-03 18:06:10 +0100

    barre de recherche

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import { GiDelicatePerfume } from 'react-icons/gi'
+import React from "react";
+import { GiDelicatePerfume } from "react-icons/gi";
+import Recherche from "./Recherche";
 
 export default function Navbar() {
-    return (
-        <header className='
+  return (
+    <header
+      className="
         sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md
-        '>
-            <div className='flex items-center gap-2 text-3xl font-semibold text-red-500'>
-                <GiDelicatePerfume size={34} />
-                <div>CESI Enchères</div>
-            </div>
-            <div>Rechercher</div>
-            <div>Login</div>
-        </header>
-    )
+        "
+    >
+      <div className="flex items-center gap-2 text-3xl font-semibold text-red-500">
+        <GiDelicatePerfume size={34} />
+        <div>CESI Enchères</div>
+      </div>
+      <Recherche />
+      <div>Login</div>
+    </header>
+  );
 }
