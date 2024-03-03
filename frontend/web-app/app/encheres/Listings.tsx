5c5312f50c3aef952f39230e7b1a3dbd8268a1fd
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-03-03 12:40:36 +0100

    ajout de la pagination, non fonctionnelle

--- frontend/web-app/app/encheres/Listings.tsx
+++ frontend/web-app/app/encheres/Listings.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import EnchereCard from "./EnchereCard";
 import { log } from "console";
 import { Enchere, PagedResult } from "@/types";
+import AppPagination from "../components/AppPagination";
 
 async function getData(): Promise<PagedResult<Enchere>> {
-  const res = await fetch("http://localhost:6001/recherche?pageSize=10");
+  const res = await fetch("http://localhost:6001/recherche?pageSize=4");
 
   if (!res.ok) throw new Error("Erreur lors de la récupération des données");
 
@@ -14,11 +15,16 @@ async function getData(): Promise<PagedResult<Enchere>> {
 export default async function Listings() {
   const data = await getData();
   return (
-    <div className="grid grid-cols-4 gap-6">
-      {data &&
-        data.results.map((enchere) => {
-          return <EnchereCard enchere={enchere} key={enchere.id} />;
-        })}
-    </div>
+    <>
+      <div className="grid grid-cols-4 gap-6">
+        {data &&
+          data.results.map((enchere) => {
+            return <EnchereCard enchere={enchere} key={enchere.id} />;
+          })}
+      </div>
+      <div className="flex justify-center mt-4">
+        <AppPagination currentPage={1} pageCount={data.pageCount} />
+      </div>
+    </>
   );
 }
