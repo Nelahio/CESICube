f7ddeee537a8953ac00dcc2ab0e3aa30758c90f0
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-03-05 08:40:11 +0100

    détails session

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { GiDelicatePerfume } from "react-icons/gi";
 import Recherche from "./Recherche";
 import Logo from "./Logo";
 import LoginButton from "./LoginButton";
+import { getCurrentUser } from "../actions/authActions";
+import UserActions from "./UserActions";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user = await getCurrentUser();
   return (
     <header
       className="
@@ -13,7 +15,7 @@ export default function Navbar() {
     >
       <Logo />
       <Recherche />
-      <LoginButton />
+      {user ? <UserActions /> : <LoginButton />}
     </header>
   );
 }
