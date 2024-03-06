4ad48a4f8c7aa3acc3b471436bde0e1807e79f2b
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-03-06 14:54:04 +0100

    component input et refacto

--- frontend/web-app/app/encheres/EnchereForm.tsx
+++ frontend/web-app/app/encheres/EnchereForm.tsx
@@ -3,10 +3,11 @@
 import { Button, TextInput } from "flowbite-react";
 import React from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import Input from "../components/Input";
 
 export default function EnchereForm() {
   const {
-    register,
+    control,
     handleSubmit,
     setFocus,
     formState: { isSubmitting, isValid, isDirty, errors },
@@ -18,29 +19,24 @@ export default function EnchereForm() {
 
   return (
     <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-3 block">
-        <TextInput
-          {...register("make", { required: "La marque est obligatoire" })}
-          placeholder="Marque"
-          color={errors?.make && "Erreur"}
-          helperText={errors.make?.message as string}
-        />
-      </div>
-      <div className="mb-3 block">
-        <TextInput
-          {...register("productName", { required: "Le nom est obligatoire" })}
-          placeholder="Nom"
-          color={errors?.productName && "Erreur"}
-          helperText={errors.productName?.message as string}
-        />
-      </div>
+      <Input
+        label="Marque"
+        name="make"
+        control={control}
+        rules={{ required: "La marque est obligatoire" }}
+      />
+      <Input
+        label="Nom"
+        name="productName"
+        control={control}
+        rules={{ required: "Le nom est obligatoire" }}
+      />
       <div className="flex justify-between">
         <Button outline color="gray">
           Annuler
         </Button>
         <Button
           isProcessing={isSubmitting}
-          disabled={!isValid}
           type="submit"
           outline
           color="success"
