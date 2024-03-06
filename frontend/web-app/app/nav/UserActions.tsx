cbe87620ef606bffcd176dcb4970f7efe96322a0
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-03-06 13:11:37 +0100

    récupération des enchères remportées

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { useParamsStore } from "@/hooks/useParamsStore";
 import { Dropdown } from "flowbite-react";
 import { User } from "next-auth";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import {
   AiFillDollarCircle,
@@ -12,17 +14,31 @@ import {
 import { HiUser, HiCog } from "react-icons/hi";
 
 type Props = {
-  user: Partial<User>;
+  user: User;
 };
 
 export default function UserActions({ user }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const setParams = useParamsStore((state) => state.setParams);
+
+  function setWinner() {
+    setParams({ winner: user.username, seller: undefined });
+    if (pathname !== "/") router.push("/");
+  }
+
+  function setSeller() {
+    setParams({ seller: user.username, winner: undefined });
+    if (pathname !== "/") router.push("/");
+  }
+
   return (
     <Dropdown label={`Bienvenue ${user.name}`} inline>
-      <Dropdown.Item icon={HiUser}>
-        <Link href={"/"}>Mes enchères</Link>
+      <Dropdown.Item icon={HiUser} onClick={setSeller}>
+        Mes enchères
       </Dropdown.Item>
-      <Dropdown.Item icon={AiFillTrophy}>
-        <Link href={"/"}>Enchères remportées</Link>
+      <Dropdown.Item icon={AiFillTrophy} onClick={setWinner}>
+        Enchères remportées
       </Dropdown.Item>
       <Dropdown.Item icon={AiFillDollarCircle}>
         <Link href={"/encheres/create"}>Vendre un produit</Link>
