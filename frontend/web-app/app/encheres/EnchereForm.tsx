86180e4be17b5ad55a1df58b93c63d2375de722a
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-03-06 13:18:40 +0100

    début formulaire création des encheres

---
+++ frontend/web-app/app/encheres/EnchereForm.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EnchereForm() {
+  return <div>Formulaire enchere</div>;
+}
--- frontend/web-app/app/encheres/create/page.tsx
+++ frontend/web-app/app/encheres/create/page.tsx
@@ -1,5 +1,15 @@
+import Heading from "@/app/components/Heading";
 import React from "react";
+import EnchereForm from "../EnchereForm";
 
 export default function Create() {
-  return <div>Créer une enchère</div>;
+  return (
+    <div className="mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg">
+      <Heading
+        title="Vendre un produit"
+        subtitle="Saisissez les détails de votre produit"
+      />
+      <EnchereForm />
+    </div>
+  );
 }
