1981913b6aad855059c2f0f848ae7ce3d8045542
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-03-06 09:43:11 +0100

    test authent

--- frontend/web-app/app/actions/enchereActions.ts
+++ frontend/web-app/app/actions/enchereActions.ts
@@ -9,3 +9,22 @@ export async function getData(query: string): Promise<PagedResult<Enchere>> {
 
   return res.json();
 }
+
+export async function UpdateEnchereTest() {
+  const data = {
+    size: Math.floor(Math.random() * 10000) + 1,
+  };
+
+  const res = await fetch(
+    "http://localhost:6001/encheres/47111973-d176-4feb-848d-0ea22641c31a",
+    {
+      method: "PUT",
+      headers: {},
+      body: JSON.stringify(data),
+    }
+  );
+
+  if (!res.ok) return { status: res.status, message: res.statusText };
+
+  return res.statusText;
+}
