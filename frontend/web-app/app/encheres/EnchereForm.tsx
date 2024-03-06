cb74754f48f796eb8844201bdf3ef3c9f4801d0e
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-03-06 13:33:26 +0100

    début formulaire de creation d'enchere

--- frontend/web-app/app/encheres/EnchereForm.tsx
+++ frontend/web-app/app/encheres/EnchereForm.tsx
@@ -1,5 +1,53 @@
+"use client";
+
+import { Button, TextInput } from "flowbite-react";
 import React from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 export default function EnchereForm() {
-  return <div>Formulaire enchere</div>;
+  const {
+    register,
+    handleSubmit,
+    setFocus,
+    formState: { isSubmitting, isValid, isDirty, errors },
+  } = useForm();
+
+  function onSubmit(data: FieldValues) {
+    console.log(data);
+  }
+
+  return (
+    <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3 block">
+        <TextInput
+          {...register("make", { required: "La marque est obligatoire" })}
+          placeholder="Marque"
+          color={errors?.make && "Erreur"}
+          helperText={errors.make?.message as string}
+        />
+      </div>
+      <div className="mb-3 block">
+        <TextInput
+          {...register("productName", { required: "Le nom est obligatoire" })}
+          placeholder="Nom"
+          color={errors?.productName && "Erreur"}
+          helperText={errors.productName?.message as string}
+        />
+      </div>
+      <div className="flex justify-between">
+        <Button outline color="gray">
+          Annuler
+        </Button>
+        <Button
+          isProcessing={isSubmitting}
+          disabled={!isValid}
+          type="submit"
+          outline
+          color="success"
+        >
+          Envoyer
+        </Button>
+      </div>
+    </form>
+  );
 }
