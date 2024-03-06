0b62dce57b6d1971e15959e2a8b47f2c751419dd
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-03-06 12:46:40 +0100

    récupération access token pour accéder aux data

--- frontend/web-app/app/actions/enchereActions.ts
+++ frontend/web-app/app/actions/enchereActions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { Enchere, PagedResult } from "@/types";
+import { getTokenWorkaround } from "./authActions";
 
 export async function getData(query: string): Promise<PagedResult<Enchere>> {
   const res = await fetch(`http://localhost:6001/recherche${query}`);
@@ -15,11 +16,16 @@ export async function UpdateEnchereTest() {
     size: Math.floor(Math.random() * 10000) + 1,
   };
 
+  const token = await getTokenWorkaround();
+
   const res = await fetch(
     "http://localhost:6001/encheres/47111973-d176-4feb-848d-0ea22641c31a",
     {
       method: "PUT",
-      headers: {},
+      headers: {
+        "Content-type": "application/json",
+        Authorization: "Bearer " + token?.access_token,
+      },
       body: JSON.stringify(data),
     }
   );
