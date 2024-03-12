b04b5ad48e669a564eb62d8682d39f5b0af87bdd
Nelahio <57316380+Nelahio@users.noreply.github.com>
2024-03-12 15:38:45 +0100

    reference contrats dans le ms offre et ajout du consumer

---
+++ src/OffreService/Consumers/EnchereCreatedConsumer.cs
@@ -0,0 +1,21 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace OffreService;
+
+public class EnchereCreatedConsumer : IConsumer<EnchereCreated>
+{
+    public async Task Consume(ConsumeContext<EnchereCreated> context)
+    {
+        var enchere = new Enchere
+        {
+            ID = context.Message.Id.ToString(),
+            Seller = context.Message.Seller,
+            AuctionEnd = context.Message.AuctionEnd,
+            ReservePrice = context.Message.ReservePrice
+        };
+
+        await enchere.SaveAsync();
+    }
+}
